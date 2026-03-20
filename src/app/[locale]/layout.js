223ba1eecc705e31f9befd2edadf1189ab69495f
CodeMnabe <gaspar.b.alves@gmail.com>
2026-03-20 09:42:37 +0000

    Made it so by changing a variable in vercel we can take down the website

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -3,6 +3,7 @@ import { getLocale, getMessages } from "next-intl/server";
 import { ConfirmProvider } from "../components/Confirm/ConfirmProvider";
 import NavCursor from "../components/NavCursor/NavCursor";
 import TopLoader from "./components/TopLoader/TopLoader";
+import SiteUnavailable from "../components/SiteUnavailable/SiteUnavailable";
 
 export function generateStaticParams() {
   return [{ locale: "en" }, { locale: "pt" }];
@@ -11,6 +12,17 @@ export function generateStaticParams() {
 export default async function LocaleLayout({ children }) {
   const locale = await getLocale();
   const messages = await getMessages();
+
+  const isMaintenanceMode = process.env.MAINTENANCE_MODE === "true";
+
+  if (isMaintenanceMode) {
+    return (
+      <NextIntlClientProvider locale={locale} messages={messages}>
+        <SiteUnavailable />
+      </NextIntlClientProvider>
+    );
+  }
+
   return (
     <NextIntlClientProvider locale={locale} messages={messages}>
       <TopLoader />
